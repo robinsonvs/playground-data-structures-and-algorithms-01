d722b1daa8e5a084a6cce4953e88079897a3721f
robinsonvs <robinsonvs@gmail.com>
2023-03-01 09:22:28 -0300

    first commit

---
+++ DSA/DataStructures/linkedlist/LinkedList.java
@@ -0,0 +1,45 @@
+package DSA.DataStructures.linkedlist;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println(head.value);
+    }
+
+    public void getTail() {
+        System.out.println(tail.value);
+    }
+
+    public void getLength() {
+        System.out.println(length);
+    }
+
+
+}
---
+++ DSA/DataStructures/linkedlist/Main.java
@@ -0,0 +1,12 @@
+package DSA.DataStructures.linkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(4);
+        linkedList.getHead();
+        linkedList.getTail();
+        linkedList.getLength();
+        linkedList.printList();
+    }
+}
