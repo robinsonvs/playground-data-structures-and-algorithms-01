aa353512f2caedb4f6b5f5a8af6dea1636edf28b
robinsonvs <robinsonvs@gmail.com>
2023-03-25 15:03:09 -0300

    add constructor stack

---
+++ DSA/DataStructures/stack/Main.java
@@ -0,0 +1,16 @@
+package DSA.DataStructures.stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Stack stack = new Stack(4);
+
+        stack.getTop();
+        stack.getHeight();
+
+        stack.printStack();
+
+    }
+    
+}
